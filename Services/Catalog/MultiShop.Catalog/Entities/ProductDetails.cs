2c7d7c7923d0a0bcfcd605950d961c2182d3024e
Selim Hasenekoğlu <hasenek2000@gmail.com>
2024-07-01 13:45:36 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetails
+    {
+        public int ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        
+    }
+}
