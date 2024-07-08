ebc0ab42b3862d0481c268419251e23b32047bcc
Selim Hasenekoğlu <hasenek2000@gmail.com>
2024-07-08 17:00:57 +0300

    SomeUIAdded

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+
+
+{
+    public class _HeadUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _NavbarUILayoutComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
